1a06cd613b7649186607f943ab1547cd5c5e61f2
C0d3 <apples3627@icloud.com>
2024-04-17 15:31:13 +0900

    Update 44

--- 4.cpp
+++ 4.cpp
@@ -1,41 +1,54 @@
 #include <iostream>
 #include <fstream>
-
 using namespace std;
 
-void calcf1(ofstream& file) {
-    for (int i = 1000; i <= 5000; i += 5) {
-        int result = i * 5;
-        file.write((char*)&result, sizeof(result));
+struct memoryb {
+    char memory[44];
+    
+    void* getadd(int set) {
+        return memory + set;
+    }
+};
+
+void calcf1(ofstream& file, memoryb& mem) {
+    int* i = (int*)mem.getadd(16);
+    *i = 1000;
+    int* result1 = (int*)mem.getadd(4);
+    while (*i <= 5000) {
+        *result1 = *i * 5;
+        file.write((char*)result1, sizeof(int));
+        *i += 5;
     }
 }
 
-void calcf2(ofstream& file) {
-    float a = -0.5;
-    for (int i = 0; i < 3; ++i) {
-        float result = a * a;
-        file.write((char*)&result, sizeof(result));
-        a += 0.5;
+void calcf2(ofstream& file, memoryb& mem) {
+    float* a = (float*)mem.getadd(12);
+    *a = -0.5;
+    int* i = (int*)mem.getadd(16);
+    float* result2 = (float*)mem.getadd(8);
+    for (*i = 0; *i < 3; ++(*i)) {
+        *result2 = *a * *a;
+        file.write((char*)result2, sizeof(float));
+        *a += 0.5;
     }
 }
 
 void All() {
-    ofstream file("mybin4.dat", ios::binary); 
+    ofstream file("mybin4.dat", ios::binary);
     if (!file) {
         cout << "XXXX" << endl;
-        return; 
+        return;
     }
-
-    int num1 = 1234;
-    file.write((char*)&num1, sizeof(num1));
-
-    calcf1(file);
-    calcf2(file);
-
-    file.close(); 
+    
+    memoryb mem;
+    *((int*)mem.getadd(0)) = 1234;
+    file.write((char*)mem.getadd(0), sizeof(int));
+    calcf1(file, mem);
+    calcf2(file, mem);
+    file.close();
 }
 
 int main() {
-    All(); 
+    All();
     return 0;
 }
